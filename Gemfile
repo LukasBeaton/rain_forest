5bee1275a68380d461cd49aaebbe238aa335cd79
Lukas Beaton <lbeaton@adparlor.com>
2015-10-13 10:18:11 -0400

    swaps out shoulda for rspec

--- Gemfile
+++ Gemfile
@@ -8,9 +8,11 @@ ruby '2.0.0'
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
+gem 'aws-sdk'
+
 group :development do
   gem 'aws-sdk'
-  gem "shoulda", ">= 0"
+  gem "rspec", "~> 2.8.0"
   gem "rdoc", "~> 3.12"
   gem "bundler", "~> 1.0"
   gem "jeweler", "~> 2.0.1"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,21 +14,16 @@ end
 ENV["COVERAGE"] && SimpleCov.start do
   add_filter "/.rvm/"
 end
-require 'rubygems'
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-require 'test/unit'
-require 'shoulda'
-
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rspec'
 require 'rain_forest'
 
-class Test::Unit::TestCase
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+
 end
